5866a01cb8d8400593a7b073386a04920b3f05fe
JordanAguiar <jordan301018@gmail.com>
2025-02-20 23:38:23 -0300

    02

--- func/script.js
+++ func/script.js
@@ -1,9 +1,12 @@
+var altura = document.querySelector('.altura').value;
+var imc = peso / (altura * altura);
+var resultado = document.querySelector('.resultado');
+resultado.innerText = imc.toFixed(2);
+var peso = document.querySelector('.peso').value;
+
 function calcular(){
-    var peso = document.getElementById('peso').value;
-    var altura = document.getElementById('altura').value;
-    var imc = peso / (altura * altura);
-    var resultado = document.getElementById('resultado');
-    resultado.innerText = imc.toFixed(2);
+   
+
     if(imc < 18.5){
         resultado.textContent += ' - Abaixo do peso';
     }else if(imc < 24.9){
