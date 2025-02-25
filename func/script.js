9ad1f66d7f78c83f46fb65b33eee335b5ac47651
JordanAguiar <jordan301018@gmail.com>
2025-02-25 00:24:34 -0300

    correção e funcionalidade

--- func/script.js
+++ func/script.js
@@ -1,16 +1,25 @@
-var altura = document.querySelector('.altura').value;
-var imc = peso / (altura * altura);
-var resultado = document.querySelector('.resultado');
-resultado.innerText = imc.toFixed(2);
-var peso = document.querySelector('.peso').value;
 
 function calcular(){
-   
+    var altura = document.querySelector('.altura').value;
+    var peso = document.querySelector('.peso').value;
+    var imc = peso / (altura * altura);
+    var resultado = imc.toFixed(1);
 
-    if(imc < 18.5){
-        resultado.textContent += ' - Abaixo do peso';
-    }else if(imc < 24.9){
-        resultado.textContent += ' - Peso normal';
-    }else{
-    resultado.textContent += ' - Acima do peso';}
+    if (resultado < 18.5) {
+        document.querySelector('.resultado').innerHTML = `${resultado} Abaixo do peso `;
+    } else if (resultado >= 18.5 && resultado <= 24.9) {
+        document.querySelector('.resultado').innerHTML = `${resultado} Peso normal `;
+    } else if (resultado >= 25 && resultado <= 29.9) {
+        document.querySelector('.resultado').innerHTML = `${resultado} Sobrepeso `;
+    } else if (resultado >= 30 && resultado <= 34.9 ){
+        document.querySelector('.resultado').innerHTML = `${resultado} Obesidade grau 1 `;;
+    } else if (resultado >= 35 && resultado <= 39.9) {
+        document.querySelector('.resultado').innerHTML = `${resultado} Obesidade grau 2 `;;
+    } else {
+        document.querySelector('.resultado').innerHTML = `${resultado} Obesidade grau 3 `;;
+    }
+
 }
+
+
+
